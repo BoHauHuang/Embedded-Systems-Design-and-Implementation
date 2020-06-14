704b695ff904afea8eb79168fa4aa15cf1cf69ee
bohauhuang <nctu0511097@gmail.com>
2020-06-14 14:28:54 +0800

    add record/play function

--- bt_keyboard_adapter_pi/KB_listener.py
+++ bt_keyboard_adapter_pi/KB_listener.py
@@ -2,7 +2,7 @@ import keyboard
 import time
 import pprint
 
-from . import SCAN_CODES_HID_USAGEID_MAPPING, MODIFIER
+from . import SCAN_CODES_HID_USAGEID_MAPPING, MODIFIER, NAME_MAPPING_TO_SCAN_CODES
 
 
 class KB_Listener:
@@ -10,6 +10,7 @@ class KB_Listener:
         self.modifier = 0x00
         self.key_hold = 0x00
         self.bt = bt_module
+        self.record_list = None
 
     def key_press_handler(self, key):
         if keyboard.is_modifier(key.scan_code):
@@ -18,6 +19,28 @@ class KB_Listener:
         self.bt.send_report(
             modifier=self.modifier, code=(self.key_hold, 0x00, 0x00, 0x00, 0x00, 0x00)
         )
+
+        # press F1 to record
+        if self.key_hold == 0x3A:
+            self.record_list = keyboard.record() # default: until 'esc' pressed
+
+        # press F2 to play the record
+        if self.key_hold == 0x3B:
+            for event in self.record_list[:-1]:  # discard the esc down for quiting record
+                event = event.replace("KeyboardEvent", "")
+                [k, action] = event.split(" ")
+                k = k[1:]
+                action = action[:-1]
+                k_id = SCAN_CODES_HID_USAGEID_MAPPING[NAME_MAPPING_TO_SCAN_CODES[k]]
+
+                if action == "down":
+                    if keyboard.is_modifier(key.scan_code):
+                        self.modifier |= MODIFIER[key.scan_code]
+                    self.bt.send_report(modifier=self.modifier, code=(k, 0x00, 0x00, 0x00, 0x00, 0x00))
+                else:
+                    if keyboard.is_modifier(key.scan_code):
+                        self.modifier &= ~MODIFIER[key.scan_code]
+                    self.bt.send_report(self.modifier)
 
     def key_release_handler(self, key):
         if keyboard.is_modifier(key.scan_code):
