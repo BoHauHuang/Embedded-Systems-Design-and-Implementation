2a8064c76f069515feb3278b8bc479c9f41a6437
kaiiiz <at881005@gmail.com>
2020-06-14 18:15:39 +0800

    recording feature

--- bt_keyboard_adapter_pi/KB_listener.py
+++ bt_keyboard_adapter_pi/KB_listener.py
@@ -1,15 +1,20 @@
 import keyboard
 import time
 import pprint
+import multiprocessing as mp
+import queue
 
-from . import SCAN_CODES_HID_USAGEID_MAPPING, MODIFIER, NAME_MAPPING_TO_SCAN_CODES
+from . import SCAN_CODES_HID_USAGEID_MAPPING, MODIFIER
+
 
 class KB_Listener:
     def __init__(self, bt_module):
         self.modifier = 0x00
         self.key_hold = 0x00
         self.bt = bt_module
-        self.record_list = None
+        self.recording = False
+        self.replaying = False
+        self.replay_buf = []
 
     def key_press_handler(self, key):
         if keyboard.is_modifier(key.scan_code):
@@ -18,34 +23,28 @@ class KB_Listener:
         self.bt.send_report(
             modifier=self.modifier, code=(self.key_hold, 0x00, 0x00, 0x00, 0x00, 0x00)
         )
-        #print(self.key_hold)
-        # press F1 start record
-        if self.key_hold == 0x3A:
-            keyboard.start_recording()
-        # press esc stop record
-        if self.key_hold == 0x29:
-            self.record_list = keyboard.stop_recording()[2:]
-            #print(self.record_list)
-        # press F2 to play the record
-        if self.key_hold == 0x3B:
-            keyboard.play(self.record_list)
-            '''
-            replay_modifier = 0x00
-            for event in self.record_list[:-1]:  # discard the esc down for quiting record
-                k = event.scan_code
-                event = str(event).replace("KeyboardEvent", "")
-                [k, action] = event.split(" ")
-                action = action[:-1]
+        # replaying, press F2 again to stop
+        if self.replaying:
+            if self.key_hold == 0x3B:
+                self.replaying = False
+        else:
+            # F1: start record, prevent nested recording
+            if self.key_hold == 0x3A and not self.recording and not self.replaying:
+                keyboard.start_recording()
+                self.recording = True
+            # ESC: stop record
+            elif self.key_hold == 0x29:
+                try:
+                    # the first 2 elements is F1 down, F1 up
+                    self.replay_buf = keyboard.stop_recording()[2:]
+                except ValueError:  # if not keyboard.start_recording()
+                    pass
+                finally:
+                    self.recording = False
+            # F2: play the record
+            elif self.key_hold == 0x3B:
+                self.replaying = True
 
-                if action == "down":
-                    if keyboard.is_modifier(key.scan_code):
-                        replay_modifier |= MODIFIER[key.scan_code]
-                    self.bt.send_report(modifier=replay_modifier, code=(k_id, 0x00, 0x00, 0x00, 0x00, 0x00))
-                else:
-                    if keyboard.is_modifier(key.scan_code):
-                        replay_modifier &= ~MODIFIER[key.scan_code]
-                    self.bt.send_report(replay_modifier)
-            '''
     def key_release_handler(self, key):
         if keyboard.is_modifier(key.scan_code):
             self.modifier &= ~MODIFIER[key.scan_code]
@@ -59,7 +58,14 @@ class KB_Listener:
     def listen(self):
         self.register()
         while True:
-            time.sleep(1)
+            if self.replaying:
+                child = mp.Process(
+                    target=lambda e: keyboard.replay(e), args=(self.replay_buf,)
+                )
+                child.start()
+                child.join()
+            else:
+                time.sleep(1)
 
 
 def main():
