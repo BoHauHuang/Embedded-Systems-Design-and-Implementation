dd1d0dc3aa4172f292924e47e738fcd54eb8497c
kaiiiz <at881005@gmail.com>
2020-06-18 17:29:50 +0800

    fix macro

--- bt_keyboard_adapter_pi/KB_listener.py
+++ bt_keyboard_adapter_pi/KB_listener.py
@@ -18,71 +18,50 @@ class KB_Listener:
 
         self.pre_key = 0x00
         self.macro_key = 0x00
-        self.macro_playing = False
-        self.macro_recording = False
-        self.macro_buf = []
 
     def key_press_handler(self, key):
         if keyboard.is_modifier(key.scan_code):
             self.modifier |= MODIFIER[key.scan_code]
         self.key_hold = SCAN_CODES_HID_USAGEID_MAPPING[key.scan_code]
-        self.bt.send_report(
-            modifier=self.modifier, code=(self.key_hold, 0x00, 0x00, 0x00, 0x00, 0x00)
-        )
+        if self.key_hold != self.macro_key:
+            self.bt.send_report(
+                modifier=self.modifier, code=(self.key_hold, 0x00, 0x00, 0x00, 0x00, 0x00)
+            )
 
-        # replaying, press F2 again to stop
+        # replaying, press macro again to stop
         if self.replaying:
-            if self.key_hold == 0x3B:
+            if self.key_hold == self.macro_key:
                 self.replaying = False
         else:
-            # F1: start record, prevent nested recording
-            if self.key_hold == 0x3A and not self.recording and not self.replaying:
-                keyboard.start_recording()
-                self.recording = True
-            # ESC: stop record
-            elif self.key_hold == 0x29:
-                try:
-                    # the first 2 elements is F1 down, F1 up
-                    self.replay_buf = keyboard.stop_recording()[2:]
-                except ValueError:  # if not keyboard.start_recording()
-                    pass
-                finally:
-                    self.recording = False
-            # F2: play the record
-            elif self.key_hold == 0x3B:
-                self.replaying = True
-
-
-        # macro for any key
-        if not self.replaying or not self.recording:
-            if self.macro_playing:
-                if self.key_hold == self.macro_key:
-                    self.macro_playing = False
-            else:
-                # F6: assign macro key
-                if self.pre_key == 0x39 and self.macro_key != key_hold and not self.macro_playing and not self.macro_recording:
-                    self.macro_key = key_hold
-                # F7: start record
-                elif self.pre_key == 0x40 and not self.macro_playing and not self.macro_recording:
-                    keyboard.start_recording()
-                    self.macro_recording = True
-                # ESC: stop record
-                elif self.key_hold == 0x29:
+            if self.recording:
+                # macro: stop record
+                if self.key_hold == self.macro_key and self.recording:
                     try:
-                        self.macro_buf = keyboard.stop_recording()[2:]
-                    except ValueError:
+                        # the first 2 elements is F1 down, F1 up
+                        self.replay_buf = keyboard.stop_recording()[2:]
+                    except ValueError:  # if not keyboard.start_recording()
                         pass
                     finally:
-                        self.macro_recording = False
+                        self.recording = False
+            else:
+                # F1: assign macro key
+                if self.pre_key == 0x3f:
+                    self.macro_key = self.key_hold
+                # F2: start record, prevent nested recording
+                elif self.key_hold == 0x40:
+                    keyboard.start_recording()
+                    self.recording = True
+                # macro: play the record
                 elif self.key_hold == self.macro_key:
-                    self.macro_playing = True
+                    self.replaying = True
                 
         self.pre_key = self.key_hold
 
     def key_release_handler(self, key):
         if keyboard.is_modifier(key.scan_code):
             self.modifier &= ~MODIFIER[key.scan_code]
-        self.bt.send_report(self.modifier)  # send empty data packet
+        if self.key_hold != self.macro_key:
+            self.bt.send_report(self.modifier)  # send empty data packet
 
     def register(self):
         for k in SCAN_CODES_HID_USAGEID_MAPPING:
@@ -95,12 +74,6 @@ class KB_Listener:
             if self.replaying:
                 child = mp.Process(
                     target=lambda e: keyboard.replay(e), args=(self.replay_buf,)
-                )
-                child.start()
-                child.join()
-            elif self.macro_playing and not self.replaying:
-                child = mp.Process(
-                    target=lambda e: keyboard.replay(e), args=(self.macro_buf,)
                 )
                 child.start()
                 child.join()
